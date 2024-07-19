ef812cb2da57cdcbb1bc922bb62cadd2c29059ef
Hadryan Silva <hadryan2001@gmail.com>
2024-07-18 22:33:29 -0300

    Initial Commit

---
+++ src/main/java/br/com/hadryan/notification/EasyNotificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hadryan/notification/EasyNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
